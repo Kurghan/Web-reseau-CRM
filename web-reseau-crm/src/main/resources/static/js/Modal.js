1e03af174bb23583eb1ecc83cc03c27c687a2b69
Laurent <kurghan@live.fr>
2018-02-16 13:45:16 +0100

    modification de l'edition de taches
    Change-Id: Ia8947a5bf552ee00aa35e927cea5a28b564ee10b

---
+++ web-reseau-crm/src/main/resources/static/js/Modal.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+function getTask(ID){
+	var IdRow = ID.parentNode.parentNode.rowIndex;
+	//alert(tasksTable.rows[IdRow].cells[0].innerHTML + " " + tasksTable.rows[IdRow].cells[1].innerHTML + " " + tasksTable.rows[IdRow].cells[2].innerHTML);
+	document.getElementById("editTaskName").value = tasksTable.rows[IdRow].cells[0].innerHTML;
+	document.getElementById("editTaskProgress").value = tasksTable.rows[IdRow].cells[1].innerHTML;
+	document.getElementById("editTaskComment").value = tasksTable.rows[IdRow].cells[2].innerHTML;
+	document.getElementById("editTaskID").value = tasksTable.rows[IdRow].cells[5].innerHTML;
+}
+
